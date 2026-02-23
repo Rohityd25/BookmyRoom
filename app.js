db24fa903ca673c2df603528d6796d3fdb1b7331
Rohit Yadav <rohit.ydv.dev@gmail.com>
2026-02-23 11:51:01 +0530

    Add premium homepage with hero, destinations, testimonials & CTA sections

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-const express=require("express");
-const app=express();
-const mongoose=require("mongoose");
-const path=require("path");
-const ExpressError=require("./utils/ExpressError.js")
-const ejsMate=require("ejs-mate");
-const methodOverride=require("method-override");
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const path = require("path");
+const ExpressError = require("./utils/ExpressError.js")
+const ejsMate = require("ejs-mate");
+const methodOverride = require("method-override");
 
-const session=require("express-session");
-const flash=require("connect-flash");
+const session = require("express-session");
+const flash = require("connect-flash");
 
 // const {listingSchema,reviewSchema}=require("./schema.js");
 // const wrapAsync=require("./utils/wrapAsync.js")
@@ -15,46 +15,46 @@ const flash=require("connect-flash");
 // const Listing=require("./models/listing");
 // const review=require("./models/review");
 
-const passport=require("passport");
-const LocalStrategy=require("passport-local");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
 
 
-const listing=require("./routes/listing.js");
-const review=require("./routes/review.js");
-const userRouter=require("./routes/user.js");
-const user=require("./models/user.js");
+const listing = require("./routes/listing.js");
+const review = require("./routes/review.js");
+const userRouter = require("./routes/user.js");
+const user = require("./models/user.js");
 
-const isLoggedin=require("./middleware.js");
+const isLoggedin = require("./middleware.js");
 
-const mongoose_url='mongodb://127.0.0.1:27017/wanderlust';
+const mongoose_url = 'mongodb://127.0.0.1:27017/wanderlust';
 
 async function main() {
     await mongoose.connect(mongoose_url);
 }
 
-main().then(()=>{
+main().then(() => {
     console.log("connected to database");
-}).catch((err)=>{
+}).catch((err) => {
     console.log(err);
 });
 
-app.set("view engine","ejs");
-app.set("views",path.join(__dirname,"views"));
-app.use(express.urlencoded({extended:true}));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname,"/public")));
+app.use(express.static(path.join(__dirname, "/public")));
 app.use(methodOverride("_method"));
-app.engine("ejs",ejsMate);
+app.engine("ejs", ejsMate);
 
 
-const sessionOption={
-    secret:"mysupersecretstring",
-    resave:false,
-    unInitialized:true,
-    cookie:{
-        expires:Date.now() + 7*24*60*60*1000,
-        maxAge:7*24*60*60*1000,
-        httpOnly:true,
+const sessionOption = {
+    secret: "mysupersecretstring",
+    resave: false,
+    unInitialized: true,
+    cookie: {
+        expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
     },
 };
 
@@ -68,11 +68,11 @@ passport.serializeUser(user.serializeUser());     //serializeUser is a method in
 passport.deserializeUser(user.deserializeUser());  //deserializeUser is a method in passport-local-mongoose
 
 
-app.use((req,res,next)=>{
+app.use((req, res, next) => {
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
     // console.log(res.locals.success);
-    res.locals.currUser=req.user;
+    res.locals.currUser = req.user;
     next();
 })
 
@@ -88,23 +88,23 @@ app.use((req,res,next)=>{
 // })
 
 // root route
-app.get("/",(req,res)=>{
-    res.send("This is root page...");
+app.get("/", (req, res) => {
+    res.render("home.ejs");
 })
 
-app.use("/listing",listing);
-app.use("/listing/:id/review",review);
-app.use("/",userRouter);
+app.use("/listing", listing);
+app.use("/listing/:id/review", review);
+app.use("/", userRouter);
 
 
-app.use("*",(req,res,next)=>{
-    next(new ExpressError(404,"Page not found!"));
+app.use("*", (req, res, next) => {
+    next(new ExpressError(404, "Page not found!"));
 })
 
 
-app.use((err,req,res,next)=>{
-    let {status=500,message="something went wrong!"}=err;
-    res.render("error.ejs",{message});
+app.use((err, req, res, next) => {
+    let { status = 500, message = "something went wrong!" } = err;
+    res.render("error.ejs", { message });
     // res.status(status).send(message);
     // res.send("something went wrong!");
 })
@@ -125,6 +125,6 @@ app.use((err,req,res,next)=>{
 // });
 
 
-app.listen(3000,()=>{                                       //app.listen callback is not a http request handler
+app.listen(3000, () => {                                       //app.listen callback is not a http request handler
     console.log("app is listening..");                      //can't use res.send
 })
